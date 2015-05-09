a85eab34a2dc559258df9862830e38ea0a7e1a02
Anthony Satkowski <vexteritydev@gmail.com>
2015-05-08 22:25:52 -0400

    Added: Pokemon, Reddit, and Bing scripts to the list. Also added BTOA as dependency to package.json.

---
+++ scripts/bing.js
@@ -0,0 +1,59 @@
+var btoa = require('btoa'); 
+
+var key = '5vIACF4JRhYHu9w%2FDsWewWc1fPNksfD8DbMH9o0o54g';
+var auth = new Buffer([ key, key ].join(':')).toString('base64');
+var rootURL = 'https://' + key + ':' + key + '@api.datamarket.azure.com/Bing/Search/';
+var searchType = 'Image';
+var market = '%27en-us%27';
+var safe_search = '%27Off%27';    
+
+
+
+
+module.exports = function(robot) {
+	robot.respond(/(bing)( me)? (.*)/i, function(msg) {
+		
+		var query = encodeURIComponent( "'" + msg.match[3] + "'" );
+
+		var full_url = rootURL + searchType + '?$format=json&Query=' + query + '&Market=' + market + '&Adult=' + safe_search;
+
+		// msg.send(full_url);
+
+		msg.http(full_url).get()( function(err, res, body) {
+			if( err ){
+				return msg.send( 'Error: ' + err );
+			} else {
+				if( res.statusCode != 200 ){
+					return msg.send( 'did not get 200 back: ' + res.statusCode );
+				} else {
+					if( !body || body == '' ){
+						return msg.send('wtf no stoofs?')
+					} else {
+						body = JSON.parse(body);
+						var first = body.d;
+						if( !first || first == '' ){
+							return msg.send('cant find the d');
+						} else {
+							var results = first.results;
+							if( !results || results == '' ){
+								return msg.send('cant find results');
+							} else {
+								var randy = Math.floor(Math.random() * results.length);
+								var post = results[randy];
+
+
+
+								return msg.send(post.MediaUrl);
+							}
+						}
+					}
+					
+				}
+			}
+			
+		});
+	});
+}
+
+
+
